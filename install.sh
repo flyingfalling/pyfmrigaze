966d8479780c234f11a26061ecd58287f4c78b73
Richard Veale <richard.e.veale@gmail.com>
2026-03-06 23:13:18 +0000

    REV: example for how to install

---
+++ install.sh
@@ -0,0 +1,5 @@
+python3 -m venv /scratch/pyfmrigaze_venv
+source /scratch/pyfmrigaze_venv/bin/activate
+pip install matplotlib numpy pandas
+pip install git+http://github.com/flyingfalling/peyeutils
+pip install git+http://github.com/flyingfalling/pyedfread
