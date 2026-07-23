1bca9f5eaf043865cc0fa0e3c40c963160de8df7
Richard Veale <richard.e.veale@gmail.com>
2026-07-23 15:32:21 +0900

    REV: ah, 01_summarize

---
+++ examples/gaze/py/01_summarize_trials.py
@@ -0,0 +1,59 @@
+
+
+
+import pandas as pd;
+import sys;
+import os;
+import peyeutils as pu;
+from multiprocessing import Pool;
+import numpy as np;
+
+def main():
+    rowcsv = sys.argv[1]; #REV: this is "all EDFs" (combined with subject info).
+    csvdir = sys.argv[2]; #REV: this is just dir.
+    
+    rowdf = pd.read_csv(rowcsv); #REV: e.g. allfmriedfs.csv
+    
+    #rowdf = rowdf.loc[ (rowdf['haseyetracking'] & (False==rowdf['edferror'])) ];
+    
+    for i,row in rowdf.iterrows():
+        print(row);
+        pass;
+
+    print(rowdf);
+    
+    exit(0);
+    
+    #rowdf = rowdf[:5];
+    results = list();
+    rows=[ dict(row=row,csvdir=csvdir) for i,row in rowdf.iterrows() ];
+    
+    
+    
+    
+    MULTIPROC=False; #True;
+    NPROC=48;
+    
+    if(MULTIPROC):
+        with Pool(processes=NPROC) as pool:
+            results = pool.map(process_events, rows);
+            pass;
+        pass;
+    else:
+        for rowdict in rows:
+            results.append( process_events( rowdict ) );
+            pass;
+        pass;
+    
+    rowdf = pd.DataFrame(results);
+    print(rowdf);
+
+    for i, row in rowdf.iterrows():
+        plotrow(dict(row=row, csvdir=csvdir) );
+        pass;
+    
+    return 0;
+
+if __name__=='__main__':
+    exit(main());
+
