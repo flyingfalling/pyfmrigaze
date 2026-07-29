9fb7e271d0304321c4d6a21754ef3bba8f3f9339
Richard Veale <richard.e.veale@gmail.com>
2026-07-29 23:41:26 +0000

    REV: example plotting.

--- examples/gaze/py/03_plot_singleedf.py
+++ examples/gaze/py/03_plot_singleedf.py
@@ -10,22 +10,25 @@ def main():
 
     edf = sys.argv[1];
 
+    eyes=['B'];
+
     sampcsv = edf + '.samples.csv';
     evcsv = edf + '.events2.csv';
-
+    
     sdf = pd.read_csv(sampcsv);
     ev = pd.read_csv(evcsv);
     
     tcol='Tsec';
     xcol='cgx_dva';
-    xcol='cgy_dva';
+    ycol='cgy_dva';
     pupil_col= 'pa';
+    eyecol='eye';
     
     for i,fig in enumerate(
             pu.plotting.plot_gaze_chunks_wpupil( df=sdf, timestamp_col=tcol, x_col=xcol, y_col=ycol, chunk_size_sec=10,
                                                  events_df=ev, event_start_col='stsec', event_end_col='ensec',
                                                  event_type_col='label', max_chunks_per_fig=4,
-                                                 pupil_col=pupil_col,
+                                                 pupil_col=pupil_col, eyes_to_plot=eyes, eye_col=eyecol
                                                 )
     ):
         fig.savefig('testfig_{:04d}.pdf'.format(i));
