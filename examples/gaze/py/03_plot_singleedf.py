cbeaf306a6d7f37a8f73334f0887b24c629222df
Richard Veale <richard.e.veale@gmail.com>
2026-07-30 07:52:10 +0900

    REV: example plot.

---
+++ examples/gaze/py/03_plot_singleedf.py
@@ -0,0 +1,38 @@
+
+import pandas as pd
+import numpy as np
+import peyeutils as pu
+
+import sys;
+import os;
+
+def main():
+
+    edf = sys.argv[1];
+
+    sampcsv = edf + '.samples.csv';
+    evcsv = edf + '.events2.csv';
+
+    sdf = pd.read_csv(sampcsv);
+    ev = pd.read_csv(evcsv);
+    
+    tcol='Tsec';
+    xcol='cgx_dva';
+    xcol='cgy_dva';
+    pupil_col= 'pa';
+    
+    for i,fig in enumerate(
+            pu.plotting.plot_gaze_chunks_wpupil( df=sdf, timestamp_col=tcol, x_col=xcol, y_col=ycol, chunk_size_sec=10,
+                                                 events_df=ev, event_start_col='stsec', event_end_col='ensec',
+                                                 event_type_col='label', max_chunks_per_fig=4,
+                                                 pupil_col=pupil_col,
+                                                )
+    ):
+        fig.savefig('testfig_{:04d}.pdf'.format(i));
+        pass;
+    
+    return 0;
+
+if __name__=='__main__':
+    exit(main());
+    pass;
